31111bb4891c6edf4d0a938b88745254ddc6cc8c
evans-kithinji <evanskithinji551@gmail.com>
2023-10-15 00:00:08 -0700

    function that prints the n times table, starting with 0. If n is greater than 15 or less than 0 the function should not print anything

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,23 +9,23 @@ void print_times_table(int n)
 {
 int i, j, result;
 
-if (n >= 0 && n <= 15) 
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 result = i * j;
-if (j == 0) 
+if (j == 0)
 {
-if (i == 0) 
+if (i == 0)
 _putchar(result + '0');
-else 
+else
 {
 _putchar('0');
 }
-} 
-else 
+}
+else
 {
 _putchar(',');
 _putchar(' ');
