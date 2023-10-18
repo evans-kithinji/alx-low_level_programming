cad91984a34b781d003c7e52d6fd910bd7af3663
evans-kithinji <evanskithinji551@gmail.com>
2023-10-18 04:29:03 -0700

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a triangle
+ * @size: the size of the triangle
+ * Return: void
+*/
+void print_triangle(int size)
+{
+int row, hash, space;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (space = size - row; space >= 1; space--)
+{
+_putchar(' ');
+}
+for  (hash = 1; hash <= row; hash++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
