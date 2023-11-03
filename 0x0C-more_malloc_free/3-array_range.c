af70517639ef58eda4668b4101eeca394d1bf24d
evans-kithinji <evanskithinji551@gmail.com>
2023-11-03 00:44:45 -0700

    creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,53 +1,30 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * array_range - Creates an array of integers from min to max (inclusive).
- * @min: The minimum value of the array.
- * @max: The maximum value of the array.
+ * array_range - creates an array of integers from min to max
+ * @min: the minimum value (inclusive)
+ * @max: the maximum value (inclusive)
  *
- * Return: Pointer to the newly created array or NULL on failure.
+ * Return: pointer to the newly created array or NULL on failure
  */
 int *array_range(int min, int max)
 {
-int *arr;
+int *result;
 int size, i;
 
 if (min > max)
 return (NULL);
 
 size = max - min + 1;
-arr = malloc(sizeof(int) * size);
+result = malloc(size * sizeof(int));
 
-if (arr == NULL)
+if (result == NULL)
 return (NULL);
 
 for (i = 0; i < size; i++)
-arr[i] = min++;
-
-return (arr);
+{
+result[i] = min + i;
 }
 
-#include "main.h"
-#include <stdio.h>
-
-int main(void)
-{
-int *a;
-int i;  /*Declare 'i' outside of the for loop.*/
-
-a = array_range(0, 10);
-if (a == NULL)
-{
-printf("Memory allocation failed.\n");
-return (1);
-}
-
-for (i = 0; i <= 10; i++)  /*Use 'i' within the for loop.*/
-printf("0x%02x ", a[i]);
-printf("\n");
-
-free(a);
-return (0);
+return (result);
 }
--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -1,9 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Test the array_range function.
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -12,16 +41,7 @@ int main(void)
     int *a;
 
     a = array_range(0, 10);
-    if (a == NULL)
-    {
-        printf("Memory allocation failed.\n");
-        return (1);
-    }
-
-    for (int i = 0; i <= 10; i++)
-        printf("0x%02x ", a[i]);
-    printf("\n");
-
+    simple_print_buffer(a, 11);
     free(a);
     return (0);
 }
